4dac6ab59b2344e743d5eee4ce7bce1b2d0ee807
ArmannGr <grewal.armann15@gmail.com>
2024-05-18 00:21:49 +0200

    feat(home): add reviews and google maps integration

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  reviews = [
+    {
+      author_name: 'Veselin Kolev',
+      text: 'Super Essen! Die Besitzer sind super freundlich und gehen auf jeden Kundenwunsch ein. Die Portionen sind perfekt. ...',
+      rating: '5/5'
+    },
+    {
+      author_name: 'Stefan Sobolewski',
+      text: 'Auch wenn es recht unscheinbar wirkt, das Essen ist sehr zu empfehlen. Indisch  authentisch gegessen, gute Schärfe, sehr gute Beilagen. ...',
+      rating: '5/5'
+    },
+    {
+      author_name: 'Thomas Imhof',
+      text: 'Erst etwas skeptisch, da der Italiener doch recht nach Indien aussieht. Aber Pizza war gut. ...',
+      rating: '4/5'
+    }
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
